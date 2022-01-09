a5a104f46655396d555b5ae8b1a019801e02bfb2
anclarma <anclarma@student.42.fr>
2022-01-09 12:56:44 +0100

    Makefile ok

---
+++ libunit/rush/real-tests/includes/libft_test.h
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft_test.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anclarma <anclarma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/09 12:00:16 by anclarma          #+#    #+#             */
+/*   Updated: 2022/01/09 12:51:30 by anclarma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_TEST_H
+# define LIBFT_TEST_H
+
+int	ft_isalnum_launcher(void);
+int	ft_isalnum_num(void);
+int	ft_isalnum_notnum(void);
+int	ft_isalpha_launcher(void);
+int	ft_isalpha_alpha(void);
+int	ft_isalpha_notalpha(void);
+int	ft_isascii_launcher(void);
+int	ft_isascii_ascii(void);
+int	ft_isascii_notascii(void);
+int	ft_isdigit_launcher(void);
+int	ft_isdigit_digit(void);
+int	ft_isdigit_notdigit(void);
+int	ft_isprint_launcher(void);
+int	ft_isprint_print(void);
+int	ft_isprint_notprint(void);
+int	ft_isspace_launcher(void);
+int	ft_isspace_space(void);
+int	ft_isspace_notspace(void);
+int	ft_tolower_launcher(void);
+int	ft_tolower_base(void);
+int	ft_toupper_launcher(void);
+int	ft_toupper_base(void);
+int	ft_memccpy_launcher(void);
+int	ft_memccpy_base(void);
+int	ft_memcpy_launcher(void);
+int	ft_memcpy_base(void);
+int	ft_memset_launcher(void);
+int	ft_memset_base(void);
+int	ft_strdup_launcher(void);
+int	ft_strdup_base(void);
+int	ft_strdup_null(void);
+int	ft_strlcat_launcher(void);
+int	ft_strlcat_base(void);
+int	ft_strlcpy_launcher(void);
+int	ft_strlcpy_base(void);
+int	ft_strlen_launcher(void);
+int	ft_strlen_base(void);
+int	ft_strlen_null(void);
+int	ft_bzero_launcher(void);
+int	ft_bzero_base(void);
+int	ft_atoi_launcher(void);
+int	ft_atoi_base(void);
+int	ft_calloc_launcher(void);
+int	ft_calloc_base(void);
+
+#endif
---
+++ libunit/rush/real-tests/main.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anclarma <anclarma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/08 10:32:40 by anclarma          #+#    #+#             */
+/*   Updated: 2022/01/09 12:08:14 by anclarma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libunit.h"
+#include "libft_test.h"
+
+int	main(int ac, char **av)
+{
+	int	ret;
+
+	(void)ac;
+	(void)av;
+	ret = 0;
+	ret += ft_isalnum_launcher();
+	ret += ft_isalpha_launcher();
+	ret += ft_isascii_launcher();
+	ret += ft_isdigit_launcher();
+	ret += ft_isprint_launcher();
+	ret += ft_isspace_launcher();
+	ret += ft_tolower_launcher();
+	ret += ft_toupper_launcher();
+	ret += ft_memccpy_launcher();
+	ret += ft_memcpy_launcher();
+	ret += ft_memset_launcher();
+	ret += ft_strdup_launcher();
+	ret += ft_strlcat_launcher();
+	ret += ft_strlcpy_launcher();
+	ret += ft_strlen_launcher();
+	ret += ft_strlen_launcher();
+	ret += ft_bzero_launcher();
+	ret += ft_atoi_launcher();
+	ret += ft_calloc_launcher();
+	print_counter();
+	if (ret == 0)
+		return (0);
+	else
+		return (-1);
+}
