015d5392a6af8b963bf1cd1ed01a4c6eee5b3a7b
anclarma <anclarma@student.42.fr>
2022-02-13 17:33:36 +0100

    correction algo

--- AlCu/rush/srcs/ai_turn.c
+++ AlCu/rush/srcs/ai_turn.c
@@ -6,7 +6,7 @@
 /*   By: anclarma <anclarma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 10:06:56 by anclarma          #+#    #+#             */
-/*   Updated: 2022/02/13 12:01:25 by anclarma         ###   ########.fr       */
+/*   Updated: 2022/02/13 17:33:17 by anclarma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,6 @@
 static int	eval_line(int line)
 {
 	return (line == 1 || (line - 1) % 4 == 0);
-}
-
-static int	eval_all_line(t_list *lst_line)
-{
-	int	n;
-
-	n = 0;
-	while (lst_line && lst_line->next)
-	{
-		n += eval_line(ft_atoi(lst_line->content));
-		lst_line = lst_line->next;
-	}
-	return (n);
 }
 
 static int	ia_choice_win(int line)
@@ -47,11 +34,31 @@ static int	ia_choice_lose(int line)
 		return (line % 4);
 }
 
+static int	eval_all_line(t_list *lst_line)
+{
+	int	objectif;
+
+	objectif = 0;
+	while (lst_line && lst_line->next)
+	{
+		if (objectif == 0 && eval_line(ft_atoi(lst_line->content)) == 0)
+			objectif = 0;
+		else if (objectif == 0 && eval_line(ft_atoi(lst_line->content)) == 1)
+			objectif = 1;
+		else if (objectif == 1 && eval_line(ft_atoi(lst_line->content)) == 0)
+			objectif = 0;
+		else if (objectif == 1 && eval_line(ft_atoi(lst_line->content)) == 1)
+			objectif = 0;
+		lst_line = lst_line->next;
+	}
+	return (objectif);
+}
+
 int	ai_turn(t_list **lst_line)
 {
 	int	move;
 
-	if (ft_lstsize(*lst_line) == 1 || eval_all_line(*lst_line) % 2 == 0)
+	if (ft_lstsize(*lst_line) == 1 || eval_all_line(*lst_line) == 0)
 		move = ia_choice_win(ft_atoi(ft_lstlast(*lst_line)->content));
 	else
 		move = ia_choice_lose(ft_atoi(ft_lstlast(*lst_line)->content));
